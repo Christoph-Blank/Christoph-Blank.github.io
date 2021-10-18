1e58cb475c8b4f90d304fa75900f4111b6f96311
Christoph-Blank <91676740+Christoph-Blank@users.noreply.github.com>
2021-10-18 13:35:56 +0200

    Add files via upload

---
+++ Info_Karte/config.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+
+const Config = {
+    wfsImgPath: "../Info_Karte/resources/img/",
+    namedProjections: [[
+        "EPSG:25833", "+title=ETRS89/UTM 33N +proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+        ]],
+    footer: {
+        urls: [{
+            "bezeichnung": "Kartographie und Gestaltung: ",
+            "url": "https://www.berlin.de/ba-mitte/politik-und-verwaltung/aemter/stadtentwicklungsamt/kataster-und-vermessung/",
+            "alias": "Bezirksamt Mitte von Berlin, Fachbereich Vermessung",
+            "alias_mobil": "BA Mitte, Verm"
+            }],
+        showVersion: true
+        },
+    quickHelp: {
+        imgPath: "./resources/img/"
+        },
+    layerConf: "../Info_Karte/resources/services-internet.json",
+    restConf: "../Info_Karte/resources/rest-services-internet.json",
+    styleConf: "../Info_Karte/resources/style_v3.json",
+    scaleLine: true,
+    mouseHover: {
+        numFeaturesToShow: 5,
+        infoText: "(weitere Objekte. Bitte zoomen.)"
+        },
+    useVectorStyleBeta: true
+    };
+
+// conditional export to make config readable by e2e tests
+if (typeof module !== "undefined") {
+    module.exports = Config;
+                                   }
